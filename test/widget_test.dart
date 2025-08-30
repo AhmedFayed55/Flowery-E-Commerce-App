a72c84516eaa8126d5a81c72f006a60e45d4c280
Ahmed Yehia <amedyhia23@gmail.com>
2025-08-30 20:15:51 +0300

    fix : unit test action and test precommit hook

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,6 +12,12 @@ import 'package:flowers_ecommerce_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(FlowersEcommerce());
 
+    expect(find.text('0'), findsOneWidget);
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    expect(find.text('42'), findsOneWidget);
   });
 }
