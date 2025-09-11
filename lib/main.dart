34858e013eeffcb932eb976a988e3ae423c4d530
Yahya Mohamed <ym615318@gmail.com>
2025-09-11 05:36:29 +0300

    feat(ECOM-55) App-Distribution

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,15 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'config/routing/route_generator.dart';
 import 'config/theme/app_theme.dart';
 import 'core/l10n/translations/app_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+    WidgetsFlutterBinding.ensureInitialized();
+
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   await configureDependencies();
   await ScreenUtil.ensureScreenSize();
   runApp(const FlowersEcommerce());
