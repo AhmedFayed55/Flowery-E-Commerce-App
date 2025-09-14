d726ee0d642d73921b87253d0c853bd6e7c72ffd
Mostafa_Amer <161795892+MostafaAmer978@users.noreply.github.com>
2025-09-14 21:18:00 +0300

    feat(search_screen): Restart Search feature & add clean Architecture directories

--- lib/config/routing/route_generator.dart
+++ lib/config/routing/route_generator.dart
@@ -4,7 +4,7 @@ import 'package:flowers_ecommerce_app/features/auth/forget_password/presentation
 import 'package:flowers_ecommerce_app/features/auth/forget_password/presentation/pages/reset_password.dart';
 import 'package:flowers_ecommerce_app/features/auth/login/presentation/pages/login_screen.dart';
 import 'package:flowers_ecommerce_app/features/auth/register/presentation/pages/register_screen.dart';
-import 'package:flowers_ecommerce_app/features/search/textfield_screen.dart';
+import 'package:flowers_ecommerce_app/features/search/presentation/pages/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../features/auth/change_password/presentation/presentation/pages/reset_password_screen.dart';
@@ -44,7 +44,7 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (context) => const MainLayout());
 
       case AppRoutes.searchScreen:
-        return MaterialPageRoute(builder: (context) => const TextFiledScreen());
+        return MaterialPageRoute(builder: (context) => SearchScreen());
 
       case AppRoutes.changePassword:
         return MaterialPageRoute(builder: (_) => const ChangePasswordScreen());
